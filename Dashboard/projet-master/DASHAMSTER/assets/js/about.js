05062e116ec315b51527de5678f74b46230185cd
alexisdacostapereira <71263936+alexisdacostapereira@users.noreply.github.com>
2021-12-29 11:12:36 +0100

    Re-upload
    Re-upload

---
+++ Dashboard/projet-master/DASHAMSTER/assets/js/about.js
@@ -0,0 +1,19 @@
+setInterval(
+    function() {
+        $.ajax({
+            type: 'GET',
+            url: "http://localhost:8080/about.JSON",
+            success: function(value) {
+                let ip = "";
+                $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
+                    ip = data;
+                });
+                client = '"client:"{' + '"host":' + ip + "}";
+                $(".main").html(client + '"server:"{' + value + "}")
+            },
+            error: function(e) {
+                myJSON = "None";
+                $(".main").html(myJSON)
+            }
+        });
+    }, 3000);
